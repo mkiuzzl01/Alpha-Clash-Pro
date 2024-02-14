ff6d34b996858e8b83db0590e3da34ac57760a45
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-02-15 02:28:55 +0600

    the project is final

--- script/script.js
+++ script/script.js
@@ -26,11 +26,70 @@ function setBackgroundColor(elementID){
     return setBackgroundColor;
 }
 
+function removeBackgroundColor(elementID){
+    const getID = document.getElementById(elementID);
+    const removeBackgroundColor = getID.classList.remove('bg-orange-400');
+    return removeBackgroundColor;
+}
+
+function getElementValue(value){
+    const element = document.getElementById(value);
+    const ElementValue = element.innerText;
+    const newValue = parseInt(ElementValue);
+    return newValue;
+}
+function setElementValue(element,value){
+    const setElementValue = document.getElementById(element);
+    setElementValue.innerText = value;
+}
+
+
+//those function actually working inside the game;
+function keyPress(event){
+    const gamerPressKey = event.key;
+
+    //expected Press key
+    const currentAlphabet = document.getElementById('showElement');
+    const TargetPressKey = currentAlphabet.innerText;
+    const expectedPressKey = TargetPressKey.toLocaleLowerCase();
+
+
+    if (gamerPressKey === expectedPressKey){
+        removeBackgroundColor(expectedPressKey);
+        gameLoop();
+        //score update
+        const setScore = getElementValue('score') + 1;
+        //show the score
+        setElementValue('score',setScore);
+    }else{
+        //decrease life
+        const newLife = getElementValue('life') - 1;
+        //show current life
+        setElementValue('life',newLife);
+
+        if (newLife <= 0){
+            gameOver();
+        }
+    }
+
+    
+}
+//Game running
+document.addEventListener('keyup',keyPress);
+
 //open game interface
 function playNow(){
-  addClass('paly')
-  removeClass('palyGround')
-  gameLoop()
+//hide everything without playground
+  addClass('paly');
+  addClass('scoreBoard');
+
+//initial value before start game
+  setElementValue('life',5);
+  setElementValue('score',0);
+
+  removeClass('palyGround');
+  gameLoop();
+
 
 }
 
@@ -40,4 +99,15 @@ function gameLoop(){
     const show = showElement.innerText = randomAlphabet();
     setBackgroundColor(show);
    
+}
+
+//game over function
+function gameOver(){
+    addClass('palyGround');
+    removeClass('scoreBoard');
+
+    // show the total score
+    const totalScore = getElementValue('score');
+    console.log(totalScore);
+    setElementValue('total-Score',totalScore);
 }
